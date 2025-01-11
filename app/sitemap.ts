43a6ee68da9f91d3b309bb6aaa288b9a792f40bb
yusuke1816 <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-01-11 16:02:10 +0900

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,10 +1,10 @@
 import { MetadataRoute } from 'next';
-import { getAllCategoryList, getAllNewsList } from './_libs/microcms';
+import { getAllCategoryList, getBlogList } from './_libs/microcms';
 
 const buildUrl = (path?: string) => `http://localhost:3000${path ?? ''}`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const newsContents = await getAllNewsList();
+  const newsContents = await getBlogList();
   const categoryContents = await getAllCategoryList();
 
   const newsUrls: MetadataRoute.Sitemap = newsContents.map((content) => ({
